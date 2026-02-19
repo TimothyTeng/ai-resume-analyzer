b4a34c5471b842e3f2f3846cc126bf4bb791ea15
TimothyTeng <ttgm245@gmail.com>
2026-02-19 23:02:58 +0800

    Resume details page

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,7 +1,37 @@
-const Summary = () => {
-  return (
-    <div>
+import ScoreGauge from '~/components/ScoreGauge';
 
+const Category = ({title, score}: {title:string, score:number}) => {
+  const textColor = score > 70 ? 'text-green-600' : score > 49 ? 'text-yellow-600' : 'text-red-600';
+  return (
+    <div className="resume-summary">
+      <div className="category">
+        <div className="flex flex-row gap-1 items-center justify-center">
+          <p className="text-sm">{title}</p>
+        </div>
+        <p className="text-sm">
+          <span className={textColor}>{score}</span>/100
+        </p>
+      </div>
+    </div>
+  );
+}
+
+const Summary = ({feedback}: {feedback:Feedback}) => {
+  return (
+    <div className="bg-white rounded-2xl shadow-md w-full">
+      <div className="flex flex-row items-center p-4 gap-8">
+        <ScoreGauge score={feedback.overallScore} />
+        <div className="flex flex-col gap-2">
+          <h2 className="font-bold text-lg">Your Resume Score</h2>
+          <p className="text-gray-500 text-sm">
+            This score is calculated based on the variables listed below
+          </p>
+        </div>
+      </div>
+      <Category title="Tone and Style" score={feedback.toneAndStyle.score} />
+      <Category title="Content" score={feedback.content.score} />
+      <Category title="Structure" score={feedback.structure.score} />
+      <Category title="Skills" score={feedback.skills.score} />
     </div>
   );
 };
